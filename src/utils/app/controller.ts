5c667c640835aa646706bf038fd173bc0012a02e
Oleg <keeperoleg26@gmail.com>
2024-05-25 10:54:02 +0300

    middlewares

--- src/utils/app/controller.ts
+++ src/utils/app/controller.ts
@@ -2,19 +2,23 @@ import 'reflect-metadata';
 
 import express, { NextFunction, Request, Response, Router } from 'express';
 
-export function Controller(routePrefix: string): ClassDecorator {
+type RouteMiddleWare = (req: Request, res: Response, next: NextFunction) => Promise<unknown> | void;
+
+export function Controller(routePrefix: string, middlewares?: RouteMiddleWare[]): ClassDecorator {
+  const controllerMiddlewares = Array.isArray(middlewares) ? middlewares : [];
   return function (target: unknown) {
-    Reflect.defineMetadata('routePrefix', routePrefix, target);
+    Reflect.defineMetadata('routePrefix', { routePrefix, controllerMiddlewares }, target);
   };
 }
 
-export function BaseMethood(route: string, method: string): MethodDecorator {
+export function BaseMethood(route: string, method: string, middlewares?: RouteMiddleWare[]): MethodDecorator {
+  const routeMiddlewares = Array.isArray(middlewares) ? middlewares : [];
   return function (target: unknown, propertyKey: string, descriptor: PropertyDescriptor) {
-    Reflect.defineMetadata('route', { route, method }, target, propertyKey);
+    Reflect.defineMetadata('route', { route, method, routeMiddlewares }, target, propertyKey);
     const originalMethod = descriptor.value;
     descriptor.value = async function (req: Request, res: Response, next: NextFunction) {
       try {
-        const result = await originalMethod(req, res, next);
+        const result = await originalMethod.call(this, req, res, next);
         return res.json(result);
       } catch (error) {
         next(error);
@@ -22,38 +26,56 @@ export function BaseMethood(route: string, method: string): MethodDecorator {
     };
   };
 }
-export function Get(route: string): MethodDecorator {
-  return BaseMethood(route, 'get');
+export function Get(route: string, middlewares?: RouteMiddleWare[]): MethodDecorator {
+  return BaseMethood(route, 'get', middlewares);
 }
-export function Post(route: string): MethodDecorator {
-  return BaseMethood(route, 'post');
+export function Post(route: string, middlewares?: RouteMiddleWare[]): MethodDecorator {
+  return BaseMethood(route, 'post', middlewares);
 }
-export function Put(route: string): MethodDecorator {
-  return BaseMethood(route, 'put');
+export function Put(route: string, middlewares?: RouteMiddleWare[]): MethodDecorator {
+  return BaseMethood(route, 'put', middlewares);
 }
-export function Patch(route: string): MethodDecorator {
-  return BaseMethood(route, 'patch');
+export function Patch(route: string, middlewares?: RouteMiddleWare[]): MethodDecorator {
+  return BaseMethood(route, 'patch', middlewares);
 }
-export function Delete(route: string): MethodDecorator {
-  return BaseMethood(route, 'delete');
+export function Delete(route: string, middlewares?: RouteMiddleWare[]): MethodDecorator {
+  return BaseMethood(route, 'delete', middlewares);
 }
 
 export abstract class BaseController {
   private readonly router: Router = express.Router();
 
   constructor() {
-    const routePrefix = Reflect.getMetadata('routePrefix', this.constructor);
+    const { routePrefix, controllerMiddlewares } = Reflect.getMetadata('routePrefix', this.constructor) as {
+      routePrefix: string;
+      controllerMiddlewares?: RouteMiddleWare[];
+    };
+
     const prototype = Object.getPrototypeOf(this);
-    Object.getOwnPropertyNames(prototype).forEach((methodName) => {
+
+    Object.getOwnPropertyNames(prototype).forEach((methodName: string) => {
       const routeData = Reflect.getMetadata('route', prototype, methodName);
       if (routeData) {
+        const { route, method, routeMiddlewares } = routeData;
         const handler = this[methodName].bind(this);
-        this.router[routeData.method](`${routePrefix}${routeData.route}`, handler);
+
+        const methods = controllerMiddlewares
+          .concat(routeMiddlewares)
+          .map((middleware) => async (req: Request, res: Response, next: NextFunction) => {
+            try {
+              await middleware(req, res, next);
+              next();
+            } catch (error) {
+              next(error);
+            }
+          });
+
+        this.router[method](`${routePrefix}${route}`, ...methods, handler);
       }
     });
   }
 
   get route() {
-    return this.router;
+    return this.router.bind(this);
   }
 }
